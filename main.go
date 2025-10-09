eb9b83c5f4180b39e05248660088918d20a3e2e4
Max Edison <amirhossein.heidari.email@gmail.com>
2025-10-10 00:49:10 +0330

    add config.go file

--- main.go
+++ main.go
@@ -15,43 +15,6 @@ import (
 	"github.com/go-rod/rod/lib/utils"
 )
 
-type Config struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	URL      string `json:"url"`
-	Apikey   string `json:"api_key"`
-}
-
-type Meal struct {
-	Name string `json:"name"`
-}
-
-type meals struct {
-	Day       string `json:"day"`
-	Breakfast []Meal `json:"breakfast"`
-	Lunch     []Meal `json:"lunch"`
-	Dinner    []Meal `json:"dinner"`
-}
-
-type food struct {
-	Week  string  `json:"week"`
-	Meals []meals `json:"meals"`
-}
-
-func loadConfig(path string) (*Config, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var cfg Config
-	if err := json.NewDecoder(file).Decode(&cfg); err != nil {
-		return nil, err
-	}
-	return &cfg, nil
-}
-
 func send_data(page *rod.Page, cfg Config) error {
 
 	page.MustElement("#txtUsernamePlain").MustInput(cfg.Username)
